189ac22a1d3c21e19dfb64cf7c86f2f821b3805c
ehsueh2 <65674803+ehsueh2@users.noreply.github.com>
2020-06-24 22:35:13 -0400

    Project v2

---
+++ nav.js
@@ -0,0 +1,7 @@
+document.getElementById("navbar").innerHTML =
+    '<ul class="nav">' +
+    '<li class="nav"><a href="index.html">Home</a></li>' +
+    '<li class="nav"><a href="contact.html">Contact Us</a></li>' +
+    '<li class="nav"><a href="sitemap.html">Sitemap</a></li>' +
+    '<li class="nav"><a href="">Search</a></li>' +
+    '</ul>';
